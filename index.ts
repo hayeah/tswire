abded2b266161aee414fa120708ceb35cd0970d8
Howard Yeh <hayeah@gmail.com>
2024-04-14 12:16:54 +0800

    make tswire return void

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 // twire is just a skeleton function that doesn't do anything, but to make the
 // code compile, and to mark init functions for dependency injections.
-export function tswire<T>(providers: any[]): T {
-  return null as T
+export function tswire(providers: any[]): void {
+  return
 }
